95619f5ac9bffcf96cf2f7c9c835a0a606b31d93
David <davidjoelem@gmail.com>
2026-05-08 10:32:00 -0600

    Update gcl.py

--- gcl.py
+++ gcl.py
@@ -7,7 +7,7 @@ c = 1013904223
 m = 2**32 - 1
 semilla = 1
 
-#definición del generador lineal congruencial
+#Definición del generador lineal congruencial
 def glc (n , x0 = semilla ) :
 X , U = [] , []
 x = x0
@@ -23,12 +23,27 @@ X , U = glc ( n )
 print ( f" Primeros 5 valores U: {U [:5]} ")
 
 #Gráfica de la serie
-1 plt . figure ( figsize =(10 , 5) )
-2 plt . plot ( range (1 , 101) , U [:100] , ’b-’, marker =’o’,
-3 markersize =2 , linewidth =0.8)
-4 plt . xlabel (’Indice n’)
-5 plt . ylabel (’Valor de $U_n$ ’)
-6 plt . title (’Serie de datos obtenidos ’)
-7 plt . grid ( True , alpha =0.3)
-8 plt . ylim (0 , 1)
-9 plt . show ()
+plt . figure ( figsize =(10 , 5) )
+plt . plot ( range (1 , 101) , U [:100] , ’b-’, marker =’o’,
+markersize =2 , linewidth =0.8)
+plt . xlabel (’Indice n’)
+plt . ylabel (’Valor de $U_n$ ’)
+plt . title (’Serie de datos obtenidos ’)
+plt . grid ( True , alpha =0.3)
+plt . ylim (0 , 1)
+plt . show ()
+
+#Creación de las parejas ordenadas (Ui,Uj) en el cuadrado [0,1]x[0,1]
+mitad = len ( U ) // 2
+U1 = U [: mitad ]
+U2 = U [ mitad :2* mitad ]
+
+plt . figure ( figsize =(8 , 8) )
+plt . scatter ( U1 , U2 , alpha =0.5 , s =10 , c =’blue ’)
+plt . xlabel (’$U_i$ ’)
+plt . ylabel (’$U_j$ ’)
+plt . title (’Parejas $(U_i , U_j )$ en $ [0 ,1]**2 $’)
+plt . xlim (0 , 1)
+plt . ylim (0 , 1)
+plt . grid ( True , alpha =0.3)
+plt . show ()
